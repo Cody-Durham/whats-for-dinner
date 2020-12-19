ab189aeed0e176258dcfeff3d67dc86184f3d7ba
Cody Durham <codydd@gmail.com>
2020-12-19 11:26:35 -0700

    Make clear button work/ add functionality to entire meal button

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 var sideRadioButton = document.querySelector('.side-dish');
 var mainDishRadioButton = document.querySelector('.main-dish');
 var dessertRadioButton = document.querySelector('.dessert-dish');
-// var entireMealRadioButton = document.querySelector('.entire-meal');
+var entireMealRadioButton = document.querySelector('.entire-meal');
 // var clearButton = document.querySelector('.clear-button');
 var clearButton = document.querySelector('.clear-button');
+// var errorButton = document.querySelector('.error');
 
 var rightBoxContent = document.querySelector('.right-box-content');
 
 var letsCookButton = document.querySelector('.left-box-button');
-
 
 
 //Global Variables here---------------------------->
@@ -59,14 +59,13 @@ var dessert = [
   'Croissants',
   'Eclairs',
 ];
-var entireMeal = [];
+// var entireMeal = [];
 
 
 //Event Listeners here---------------------------->
-// sideRadioButton.addEventListener('click', showSide);
-
 letsCookButton.addEventListener('click', showMyMeal);
 clearButton.addEventListener('click', clearAll);
+// errorButton.addEventListener('click', showErrorMessage)
 
 
 //Functions here---------------------------->
@@ -76,31 +75,33 @@ function getRandomIndex(array) {
 
 
 function showMyMeal() {
-  // clearButton.classList.remove('.hidden');
   var randomSide = sides[getRandomIndex(sides)];
   var randomMainDish = mainDish[getRandomIndex(mainDish)];
   var randomDessert = dessert[getRandomIndex(dessert)];
-  
+  // var entireMeal = randomSide + randomMainDish + randomDessert;
 
   if (sideRadioButton.checked === true) {
     getRandomSide(randomSide);
-  } 
+  }
   if (mainDishRadioButton.checked === true) {
-     getRandomMainDish(randomMainDish);
+     getRandomMainDish(randomMainDish); 
   } 
   if (dessertRadioButton.checked === true) {
-    getRandomDessert(randomDessert) 
+    getRandomDessert(randomDessert);
+  } 
+  if (entireMealRadioButton.checked === true) {
+    getEntireMeal(randomSide, randomMainDish, randomDessert);
+  } 
+  else {
+    showErrorMessage()
   }
-
-  
+  showClearButton();
 };
 
 function getRandomSide(side) {
-  clearButton.classList.remove('hidden');
   rightBoxContent.innerHTML = `
   <h1 class="right-box-upper-message">You should make:<h1>
-  <h2 class="right-box-main-message">${side}</h2>
-  `
+  <h2 class="right-box-main-message">${side}</h2>`
 };
 
 function getRandomMainDish(main) {
@@ -112,24 +113,37 @@ function getRandomMainDish(main) {
 function getRandomDessert(dessert) {
   rightBoxContent.innerHTML = `
   <h1 class="right-box-upper-message">You should make:<h1>
-  <h2 class="right-box-main-message">${dessert}</h2>
-  `
+  <h2 class="right-box-main-message">${dessert}</h2>`
 };
+
+function getEntireMeal(side, main, dessert) {
+  rightBoxContent.innerHTML = `
+  <h1 class="right-box-upper-message">You should make:<h1>
+  <h2 class="right-box-main-message">${side}</h2>
+  <h2 class="right-box-main-message">${main}</h2>
+  <h2 class="right-box-main-message">${dessert}</h2>`
+};
+
 
 function clearAll() {
   clearButton.classList.add('hidden');
+  sideRadioButton.checked = false;
+  mainDishRadioButton.checked = false;
+  dessertRadioButton.checked = false;
   rightBoxContent.innerHTML = `
     <section class="right-box-content">
       <img id="right-img" src="./assets/cookpot.svg">
     </section>`
-
-    console.log('test');
-    sideRadioButton.checked = false;
-    mainDishRadioButton.checked = false;
-    dessertRadioButton.checked = false; 
+    console.log('test'); 
 };
 
+function showClearButton() {
+  clearButton.classList.remove('hidden');
+};
 
-
-
-
+function showErrorMessage() {
+  if (sideRadioButton.checked === false && mainDishRadioButton.checked === false && dessertRadioButton.checked === false) {
+    rightBoxContent.innerHTML = `
+    <p>Need to select an option first</p>`
+  }
+};
